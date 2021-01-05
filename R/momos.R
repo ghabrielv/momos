1aa9d790704357044f4bc63ed177d78ef4cb910e
Ghabriel Villarreal <ghabrielv@gmail.com>
2021-01-05 18:08:42 -0500

    Change if else for ifelse

--- R/momos.R
+++ R/momos.R
@@ -11,26 +11,26 @@
 momos <- function (params) {
 
   # Initial values
-  Necromasa <- if("Necromasa" %in% names(params)) params$Necromasa else 2140
-  HLo <- if("HLo" %in% names(params)) params$HLo else 2250
-  HSo <- if("HSo" %in% names(params)) params$HSo else 19150
-  Co <- if("Co" %in% names(params)) params$Co else 55.40
+  Necromasa <- ifelse("Necromasa" %in% names(params), params$Necromasa, 2140)
+  HLo <- ifelse("HLo" %in% names(params), params$HLo, 2250)
+  HSo <- ifelse("HSo" %in% names(params), params$HSo, 19150)
+  Co <- ifelse("Co" %in% names(params), params$Co, 55.40)
 
   # Parameters of the MOMOS model
-  Kvl <- if("Kvl" %in% names(params)) params$Kvl else 0.2070
-  Kvs <- if("Kvs" %in% names(params)) params$Kvs else 0.00057
-  fs <- if("fs" %in% names(params)) params$fs else 0.00002
-  Khl <- if("Khl" %in% names(params)) params$Khl else 0.0638
-  Khs <- if("Khs" %in% names(params)) params$Khs else 0.00077
-  Khls <- if("Khls" %in% names(params)) params$Khls else 0.1581
-  Kmb <- if("Kmb" %in% names(params)) params$Kmb else 0.001
-  Kresp <- if("Kresp" %in% names(params)) params$Kresp else 0.1419
-  Ci <- if("Ci" %in% names(params)) params$Ci else 50.04
+  Kvl <- ifelse("Kvl" %in% names(params), params$Kvl, 0.2070)
+  Kvs <- ifelse("Kvs" %in% names(params), params$Kvs, 0.00057)
+  fs <- ifelse("fs" %in% names(params), params$fs, 0.00002)
+  Khl <- ifelse("Khl" %in% names(params), params$Khl, 0.0638)
+  Khs <- ifelse("Khs" %in% names(params), params$Khs, 0.00077)
+  Khls <- ifelse("Khls" %in% names(params), params$Khls, 0.1581)
+  Kmb <- ifelse("Kmb" %in% names(params), params$Kmb, 0.001)
+  Kresp <- ifelse("Kresp" %in% names(params), params$Kresp, 0.1419)
+  Ci <- ifelse("Ci" %in% names(params), params$Ci, 50.04)
 
   # Execution time
-  from <- if("from" %in% names(params)) params$from else 0
-  to <- if("to" %in% names(params)) params$to else 30
-  by <- if("by" %in% names(params)) params$by else 1
+  from <- ifelse("from" %in% names(params), params$from, 0)
+  to <- ifelse("to" %in% names(params), params$to, 30)
+  by <- ifelse("by" %in% names(params), params$by, 1)
 
   # Derivs
   derivs <- function(time, y, pars) {
